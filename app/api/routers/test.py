686c3564dc468135d256f08e1c872e17f6d95434
/Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-03-25 17:08:29 +0500

    Added Alembic with migrations in app/core/migrations 🔄📂 #DatabaseMigrations

--- app/api/routers/test.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, status
-
-router = APIRouter()
-
-
-@router.get("/", status_code=status.HTTP_200_OK, response_model=dict[str, str])
-async def read_root() -> dict[str, str]:
-    return {"message": "Hello World"}
